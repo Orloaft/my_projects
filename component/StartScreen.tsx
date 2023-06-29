2aad2b5b33fa09eb188fc6595e8e3b7faf89c9da
unknown <alexorlow17@gmail.com>
2023-06-29 01:20:48 -0400

    adjust mobile styling

--- component/StartScreen.tsx
+++ component/StartScreen.tsx
@@ -7,8 +7,9 @@ export const StartScreen = () => {
         <span> Orlow</span>
       </div>
       <div className="disclaimer">
-        <b>Disclaimer:</b> Please be patient as most of my projects are hosted
-        on eco-dynos and will require about a minute to awaken after some
+        <b>Disclaimer:</b>
+        <br></br> Please be patient as most of my projects are hosted on
+        eco-dynos and will require about a minute to awaken after some
         inactivity.
       </div>
     </>
